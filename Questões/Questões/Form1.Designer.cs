9f329edeb0c6e832fd27247c5d69ec2df77d8411
Helena Alfarth Deschamps <alfarth.helena@gmail.com>
2021-06-25 21:02:46 -0300

    Atividade

---
+++ Questões/Questões/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Questões
+{
+    partial class txtCotacaoDolar
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCotacaoDólar = new System.Windows.Forms.MaskedTextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.txtDolar = new System.Windows.Forms.MaskedTextBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCotacaoDólar
+            // 
+            this.txtCotacaoDólar.Location = new System.Drawing.Point(35, 165);
+            this.txtCotacaoDólar.Mask = "00000";
+            this.txtCotacaoDólar.Name = "txtCotacaoDólar";
+            this.txtCotacaoDólar.Size = new System.Drawing.Size(127, 27);
+            this.txtCotacaoDólar.TabIndex = 0;
+            this.txtCotacaoDólar.ValidatingType = typeof(int);
+            this.txtCotacaoDólar.Leave += new System.EventHandler(this.txtCotacaoDólar_Leave);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(35, 131);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(127, 20);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Cotação do Dólar";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.linkLabel2.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel2.Location = new System.Drawing.Point(279, 131);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(189, 20);
+            this.linkLabel2.TabIndex = 3;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Informe um valor em Dólar";
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(279, 165);
+            this.txtDolar.Mask = "00000";
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(189, 27);
+            this.txtDolar.TabIndex = 2;
+            this.txtDolar.ValidatingType = typeof(int);
+            this.txtDolar.Leave += new System.EventHandler(this.txtDolar_Leave);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.linkLabel3.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel3.Location = new System.Drawing.Point(593, 131);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(132, 20);
+            this.linkLabel3.TabIndex = 5;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Conversão em real";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 29);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCotacaoDolar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.txtCotacaoDólar);
+            this.Name = "txtCotacaoDolar";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txtCotacaoDólar;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.MaskedTextBox txtDolar;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Questões/Questões/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Questões
+{
+    public partial class txtCotacaoDolar : Form
+    {
+        double Cotacao_dolar, dolar;
+
+
+
+
+        public txtCotacaoDolar()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDolar_Leave(object sender, EventArgs e)
+        {
+            dolar = double.Parse(txtDolar.Text);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double conversao;
+            conversao = dolar * Cotacao_dolar;
+
+            MessageBox.Show($"A conversão do dólar para o real é:" + conversao);
+        }
+
+        private void txtCotacaoDólar_Leave(object sender, EventArgs e)
+        {
+            Cotacao_dolar = double.Parse(txtCotacaoDólar.Text);
+        }
+
+
+
+        
+
+       
+    }
+}
